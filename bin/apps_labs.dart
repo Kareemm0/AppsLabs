4d9a518253ca9a7dbe01d610478fd56b25bd0eec
kareem_m0hmed <km7404714@gmail.com>
2025-12-06 17:19:10 +0200

    first commit

---
+++ bin/apps_labs.dart
@@ -0,0 +1,5 @@
+import 'package:apps_labs/apps_labs.dart' as apps_labs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${apps_labs.calculate()}!');
+}
---
+++ test/apps_labs_test.dart
@@ -0,0 +1,8 @@
+import 'package:apps_labs/apps_labs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
